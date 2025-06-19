261af2a74f526585f27eee4f9b3e3d22a2044186
Enda Dwagon <endadwagon@gmail.com>
2025-06-19 20:05:19 +0100

    MergeAll.sh: Add optics and prism merging and zip output images

--- MergeAll.sh
+++ MergeAll.sh
@@ -4,7 +4,7 @@ if [ "$1" == "cleanup" ]; then
     echo "Running cleanup..."
     
     # Nuke work dirs
-    rm -rf _ap_extracted _base_extracted _update_bin extracted
+    rm -rf _extracted _base_extracted _update_bin extracted
 
     # Nuke update-related files
     CLEANUP_FILES=(
@@ -17,6 +17,9 @@ if [ "$1" == "cleanup" ]; then
         system_ext.transfer.list system_ext.new.dat system_ext.patch.dat
         super.img super.img-old system.img system_dlkm.img system_ext.img
         vendor.img vendor_dlkm.img product.img odm.img Progress.txt
+        optics.transfer.list optics.new.dat optics.patch.dat
+        prism.transfer.list prism.new.dat prism.patch.dat
+        prism.img optics.img
     )
 
     for file in "${CLEANUP_FILES[@]}"; do
@@ -63,25 +66,40 @@ if [ -z "$AP_TAR" ]; then
     echo "Error: Could not find AP*.tar.md5 in base ZIP!"
     exit 1
 fi
-
+echo
+CSC_TAR=$(find _base_extracted -type f -name "CSC*.tar.md5" | head -n 1)
+if [ -z "$CSC_TAR" ]; then
+    echo "Error: Could not find CSC*.tar.md5 in base ZIP!"
+    exit 1
+fi
 echo "Found AP package: $AP_TAR"
+echo "Found  package: $CSC_TAR"
+
+# Extract optics and prism from csc
+echo
+echo "Extracting optics.img.lz4 and prism.img.lz4 from CSC..."
+mkdir -p _extracted
+tar -xf "$AP_TAR" --no-same-owner -C _extracted optics.img.lz4 prism.img.lz4
 
 # Extract super.img.lz4 from AP
 echo
 echo "Extracting super.img.lz4 from AP..."
-mkdir -p _ap_extracted
-tar -xf "$AP_TAR" --wildcards --no-same-owner -C _ap_extracted 'super.img.lz4'
+tar -xf "$AP_TAR" --wildcards --no-same-owner -C _extracted 'super.img.lz4'
 
-if [ ! -f _ap_extracted/super.img.lz4 ]; then
+if [ ! -f _extracted/super.img.lz4 ]; then
     echo "Error: super.img.lz4 not found in AP package!"
     exit 1
 fi
 
 # De-LZ4
 echo
-echo "Decompressing super.img.lz4..."
-lz4 -d _ap_extracted/super.img.lz4 super.img
-rm -f _ap_extracted/super.img.lz4
+echo "Decompressing lz4 images..."
+lz4 -d _extracted/optics.img.lz4 optics.img
+lz4 -d _extracted/prism.img.lz4 prism.img
+lz4 -d _extracted/super.img.lz4 super.img
+rm -rf _extracted/super.img.lz4
+rm -rf _extracted/prism.img.lz4
+rm -rf _extracted/optics.img.lz4
 
 # Desparse super
 echo
@@ -111,6 +129,8 @@ REQUIRED_FILES=(
     system_dlkm.img system_dlkm.transfer.list system_dlkm.new.dat system_dlkm.patch.dat
     vendor_dlkm.transfer.list vendor_dlkm.new.dat vendor_dlkm.patch.dat
     system_ext.transfer.list system_ext.new.dat system_ext.patch.dat
+    optics.transfer.list optics.new.dat optics.patch.dat
+    prism.transfer.list prism.new.dat prism.patch.dat
 )
 
 # Move each file if it exists
@@ -118,9 +138,9 @@ echo
 echo "Moving required update files..."
 for file in "${REQUIRED_FILES[@]}"; do
     if [ -f "_update_bin/$file" ]; then
-        mv "_update_bin/$file" . || echo "Failed to move $file"
+        mv "_update_bin/$file" . || echo "Skipped $file"
     else
-        echo "Warning: $file not found in update bin"
+        echo "$file was skipped"
     fi
 done
 echo "Starting Merge..."
@@ -152,27 +172,76 @@ echo
 echo "DO NOT PANIC IF YOU SEE ANY ERRORS PAST THIS POINT, YOUR DEVICE PROBABLY DOESNT HAVE THESE PARTITIONS"
 
 # System_dlkm merge
-echo
-echo "Merging system_dlkm..."
-./BlockImageUpdate system_dlkm.img system_dlkm.transfer.list system_dlkm.new.dat system_dlkm.patch.dat
-echo "System_dlkm merge complete!"
+if [ -f system_dlkm.img ]; then
+    echo
+    echo "Merging system_dlkm..."
+    ./BlockImageUpdate system_dlkm.img system_dlkm.transfer.list system_dlkm.new.dat 		system_dlkm.patch.dat
+    echo "System_dlkm merge complete!"
+else
+    echo
+    echo "Skipping system_dlkm"
+fi
 
 # Vendor_dlkm merge
-echo
-echo "Merging vendor_dlkm..."
-./BlockImageUpdate vendor_dlkm.img vendor_dlkm.transfer.list vendor_dlkm.new.dat vendor_dlkm.patch.dat
-echo "Vendor_dlkm merge complete!"
+if [ -f vendor_dlkm.img ]; then
+    echo
+    echo "Merging vendor_dlkm..."
+    ./BlockImageUpdate vendor_dlkm.img vendor_dlkm.transfer.list vendor_dlkm.new.dat vendor_dlkm.patch.dat
+    echo "Vendor_dlkm merge complete!"
+else
+    echo
+    echo "Skipping vendor_dlkm"
+fi 
 
 #System_ext merge
+if [ -f system_ext.img ]; then
+    echo
+    echo "Merging system_ext..."
+    ./BlockImageUpdate system_ext.img system_ext.transfer.list system_ext.new.dat system_ext.patch.dat
+    echo "System_ext merge complete!"
+else
+    echo
+    echo "Skipping system_ext"
+fi
+
+if [ -f optics.img ]; then
+    echo
+    echo "Merging system_ext..."
+    ./BlockImageUpdate optics.img optics.transfer.list optics.new.dat optics.patch.dat
+    echo "Optics merge complete!"
+else
+    echo
+    echo "Skipping optics"
+fi
+
+if [ -f prism.img ]; then
+    echo
+    echo "Merging prism..."
+    ./BlockImageUpdate prism.img prism.transfer.list prism.new.dat prism.patch.dat
+    echo "Prism merge complete!"
+else
+    echo
+    echo "Skipping prism"
+fi
+
 echo
-echo "Merging system_ext..."
-./BlockImageUpdate system_ext.img system_ext.transfer.list system_ext.new.dat system_ext.patch.dat
-echo "System_ext merge complete!"
+echo "Zipping output images..."
+mkdir -p output
+MERGED_IMGS=(system.img vendor.img product.img odm.img system_ext.img system_dlkm.img vendor_dlkm.img optics.img prism.img)
+for img in "${MERGED_IMGS[@]}"; do
+    if [ -f "$img" ]; then
+        mv "$img" output/
+    fi
+done
+cd output
+zip -r9 ../Merged_Firmware.zip ./*.img >/dev/null
+cd ..
+echo "ZIP archive created: Merged_Firmware.zip"
 
 # Cleanup
 echo
 echo "Cleaning Up..."
-rm -rf _ap_extracted _base_extracted _update_bin extracted
+rm -rf _extracted _base_extracted _update_bin extracted
 
 # Nuke all the now unused update-related files
 CLEANUP_FILES=(
@@ -184,6 +253,8 @@ CLEANUP_FILES=(
     vendor_dlkm.transfer.list vendor_dlkm.new.dat vendor_dlkm.patch.dat
     system_ext.transfer.list system_ext.new.dat system_ext.patch.dat
     super.img super.img-old Progress.txt
+    optics.transfer.list optics.new.dat optics.patch.dat
+    prism.transfer.list prism.new.dat prism.patch.dat
 )
 
 for file in "${CLEANUP_FILES[@]}"; do
